81c259a0662f663b53978e338d6aee130ab6909e
Tanveer Ahmad <tanveer.sys@gmail.com>
2024-10-06 22:23:11 +0400

    fixed issue

--- Database/3-Data/4- Add-data-to-Contact-Table.sql
+++ Database/3-Data/4- Add-data-to-Contact-Table.sql
@@ -4,53 +4,73 @@ GO
 BEGIN TRANSACTION;
 
 BEGIN TRY
-
--- Insert data into Contacts
-INSERT INTO Contacts (
-    UniqueId,
-    CompanyID,
-    Name,
-    Email,
-    EmailScore,
-    Phone,
-    WorkPhone,
-    LeadLocationID,
-    LeadDivisionID,
-    LeadTitle,
-    DecisionMakingPowerID,
-    SeniorityLevelID,
-    LinkedInUrl,
-    Skills,
-    PastCompanies
-)
-SELECT
-    c.ID AS UniqueId,
-    co.Id AS CompanyID,
-    c.Name,
-    c.Email,
-    c.email_score,
-    c.Phone,
-    c.work_phone,
-    ll.Id AS LeadLocationID,
-    ld.Id AS LeadDivisionID,
-    c.lead_titles,
-    dmp.Id AS DecisionMakingPowerID,
-    sl.Id AS SeniorityLevelID,
-    c.linkedin_url,
-    c.Skills,
-    c.past_companies
-FROM tblContacts AS c
-LEFT JOIN Companies co ON co.CompanyName = c.company_name
-LEFT JOIN LeadLocations ll ON ll.Location = c.Lead_Location
-LEFT JOIN LeadDivisions ld ON ld.Division = c.lead_division
-LEFT JOIN DecisionMakingPowers dmp ON dmp.DecisionMakingPower = c.decision_making_power
-LEFT JOIN SeniorityLevels sl ON sl.SeniorityLevel = c.seniority_level
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM Contacts AS con
-    WHERE con.UniqueId = c.ID
-);
--- Commit transaction if all statements succeed
+    -- Insert data into Contacts ensuring one-to-one relationships in joins
+    INSERT INTO Contacts (
+        UniqueId,
+        CompanyID,
+        Name,
+        Email,
+        EmailScore,
+        Phone,
+        WorkPhone,
+        LeadLocationID,
+        LeadDivisionID,
+        LeadTitle,
+        DecisionMakingPowerID,
+        SeniorityLevelID,
+        LinkedInUrl,
+        Skills,
+        PastCompanies
+    )
+    SELECT
+        c.ID AS UniqueId,
+        co.Id AS CompanyID,
+        c.Name,
+        c.Email,
+        c.email_score,
+        c.Phone,
+        c.work_phone,
+        ll.Id AS LeadLocationID,
+        ld.Id AS LeadDivisionID,
+        c.lead_titles,
+        dmp.Id AS DecisionMakingPowerID,
+        sl.Id AS SeniorityLevelID,
+        c.linkedin_url,
+        c.Skills,
+        c.past_companies
+    FROM tblContacts AS c
+    LEFT JOIN (
+        SELECT CompanyName, MIN(Id) AS Id
+        FROM Companies
+        GROUP BY CompanyName
+    ) co ON co.CompanyName = c.company_name
+    LEFT JOIN (
+        SELECT Location, MIN(Id) AS Id
+        FROM LeadLocations
+        GROUP BY Location
+    ) ll ON ll.Location = c.Lead_Location
+    LEFT JOIN (
+        SELECT Division, MIN(Id) AS Id
+        FROM LeadDivisions
+        GROUP BY Division
+    ) ld ON ld.Division = c.lead_division
+    LEFT JOIN (
+        SELECT DecisionMakingPower, MIN(Id) AS Id
+        FROM DecisionMakingPowers
+        GROUP BY DecisionMakingPower
+    ) dmp ON dmp.DecisionMakingPower = c.decision_making_power
+    LEFT JOIN (
+        SELECT SeniorityLevel, MIN(Id) AS Id
+        FROM SeniorityLevels
+        GROUP BY SeniorityLevel
+    ) sl ON sl.SeniorityLevel = c.seniority_level
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM Contacts AS con
+        WHERE con.UniqueId = c.ID
+    );
+    
+    -- Commit transaction if all statements succeed
     COMMIT;
 END TRY
 BEGIN CATCH
@@ -65,4 +85,4 @@ BEGIN CATCH
         ERROR_STATE() AS ErrorState;
 END CATCH;
 
-Go
+GO
